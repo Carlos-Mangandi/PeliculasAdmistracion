ca9cf4c99dd8cfb1b12cde97b784d5e63e6de64f
Carlos Mangandí <me22001@esfe.agape.edu.sv>
2023-10-19 09:01:33 -0600

    Creación del repositorio de Peliculas.Administración

---
+++ Peliculas.AdministracionBlazor/Peliculas.AdministracionBlazor/Program.cs
@@ -0,0 +1,26 @@
+
+using Blazored.LocalStorage;
+using Peliculas.AdministracionBlazor;
+using Peliculas.AdministracionBlazor.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7047/api/") });
+
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped<UserAuth>();
+IServiceCollection serviceCollection = builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationProvider>();
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddBlazoredLocalStorage();
+
+await builder.Build().RunAsync();
+
